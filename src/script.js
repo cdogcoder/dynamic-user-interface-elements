4adf57a8f2bd18c0a738a27a47a90a676ec4ca50
cdogcoder <cdogcoder@gmail.com>
2025-06-08 09:42:33 -0700

    Implement close dropdown menu feature

--- src/script.js
+++ src/script.js
@@ -3,5 +3,11 @@ import "./styles.css";
 const dropdownMenuButton = document.querySelector(".dropdown-menu-button");
 dropdownMenuButton.addEventListener("click", () => {
     const dropdownMenu = document.querySelector(".dropdown-menu");
-    dropdownMenu.classList = "dropdown-menu visible"
+    dropdownMenu.classList = "dropdown-menu visible";
+})
+
+const closeDropdownMenuButton = document.querySelector(".close-dropdown-menu-button");
+closeDropdownMenuButton.addEventListener("click", () => {
+    const dropdownMenu = document.querySelector(".dropdown-menu");
+    dropdownMenu.classList = "dropdown-menu invisible";
 })
