21210943fc23b48df5a8966841b0d3d102aed235
cdogcoder <cdogcoder@gmail.com>
2025-06-08 10:17:26 -0700

    Add class to hide dropdown menu on initial load

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ const dropdownMenuButton = document.querySelector(".dropdown-menu-button");
 dropdownMenuButton.addEventListener("click", () => {
     const dropdownMenu = document.querySelector(".dropdown-menu");
     dropdownMenu.classList = "dropdown-menu visible";
+    
 })
 
 const closeDropdownMenuButton = document.querySelector(".close-dropdown-menu-button");
