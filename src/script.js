f88ec8ec11d3d5857f83c3ad971920cb0b21f1b0
cdogcoder <cdogcoder@gmail.com>
2025-06-15 18:54:29 -0700

    Implement forward functionality of image carousel

--- src/script.js
+++ src/script.js
@@ -11,3 +11,20 @@ closeDropdownMenuButton.addEventListener("click", () => {
     const dropdownMenu = document.querySelector(".dropdown-menu");
     dropdownMenu.classList = "dropdown-menu invisible";
 })
+
+const rightArrow = document.querySelector(".right-arrow");
+rightArrow.addEventListener("click", () => {
+    const imageAnimationNames = ["tertiaryExpand", "secondaryExpand", "primaryShrink", "secondaryShrink", "tertiaryShrink"]
+    const images = document.querySelectorAll(".images *")
+    const imageHolder = document.querySelector(".images");
+    for (let i = 0; i < images.length; i++) {
+        images[i].style.setProperty("animation-name", imageAnimationNames[i]);
+    }
+    imageHolder.removeChild(images[images.length-1]);
+    const newImage = document.createElement("div");
+    newImage.classList = "image blue";
+    newImage.style.cssText = "height: 000px; flex: 0;"
+    imageHolder.prepend(newImage);
+    newImage.style.setProperty("animation-name", "defaultExpand");
+    console.log(images)
+})
